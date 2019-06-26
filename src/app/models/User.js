c60bbc8fa6cf479501fddb240dee0f0be5e0b10d
João R C Almeida <joaorca@gmail.com>
2019-06-25 23:16:14 -0300

    Aula 11

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -4,23 +4,27 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    password: DataTypes.VIRTUAL,
-    password_hash: DataTypes.STRING,
-    provider: DataTypes.BOOLEAN
-  },
-  {
-    hooks: {
-      beforeSave: async user => {
-        if (user.password) {
-          user.password_hash = await bcrypt.hash(user.password, 8)
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN
+    },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8)
+          }
         }
       }
     }
-  }
   )
+
+  User.prototype.checkPassword = function (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
 
   return User
 }
