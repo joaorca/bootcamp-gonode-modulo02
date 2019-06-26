b482a5cf68ae9ff385cc8dd569fe9f9837fb1bfe
João R C Almeida <joaorca@gmail.com>
2019-06-25 23:41:40 -0300

    Aula 14

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ class App {
       store: new LokiStore({
         path: path.resolve(__dirname, '..', 'tmp', 'sessions.db')
       }),
+      name: 'root',
       secret: 'MyAppSecret',
       resave: false,
       saveUnitialize: true
